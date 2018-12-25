97a0e0dcbfd857af257a84c4cc12a759ee9e9009
psynosaur <psynosaur@gmail.com>
2018-12-25 10:35:02 +0200

    README updated

--- tests/test_mrf.py
+++ tests/test_mrf.py
@@ -8,11 +8,12 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 class TestMRFs(unittest.TestCase):
 
     @classmethod
-    def mockdevice(cls):
+    def mockdevice(cls, vid=mrf.MoRFeus.vendorID, pid=mrf.MoRFeus.productID, index=0):
         # hard code the opening of the morfeus device and return
+        mrfdevice = hid.enumerate(vid, pid)[index]
         td = hid.device()
-        td.open(mrf.MoRFeus.vendorID, mrf.MoRFeus.productID)
-        td.set_nonblocking(1)
+        td.open_path(mrfdevice['path'])
+        td.set_nonblocking(0)
         return td
 
     def test_find(self):
@@ -22,15 +23,19 @@ class TestMRFs(unittest.TestCase):
 
     def test_initdevice(self):
         # init two test devices, one from morfeus class and another from mockdevice
-        td1 = mrf.MoRFeus.initdevice()
-        td2 = self.mockdevice()
-        # Check types
-        self.assertEqual(type(td1), type(td2))
-        # Check if 'td1' is and instance of type 'td2'(hard opened hid.device)
-        self.assertIsInstance(td1, type(td2))
-        # Close the devices
-        td1.close()
-        td2.close()
+        devicecount = mrf.MoRFeus.find()
+        devices = []
+        testdevices = []
+        for i in range(0, devicecount):
+            devices.append(mrf.MoRFeus.initdevice(i))
+            # testdevices.append(self.mockdevice(i))
+            # Check types
+            self.assertEqual(type(devices), type(testdevices))
+            # Check if 'td1' is and instance of type 'td2'(hard opened hid.device)
+            # self.assertIsInstance(td1, type(td2))
+            # Close the devices
+            # td1.close()
+            # td2.close()
 
 
 if __name__ == "__main__":
